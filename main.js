e98959599920a191e3592ac534a268850804e0f3
hao <qwe48978@gmail.com>
2023-08-28 14:38:00 +0800

    檔案上傳

---
+++ main.js
@@ -0,0 +1,19 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// aos
+AOS.init();
